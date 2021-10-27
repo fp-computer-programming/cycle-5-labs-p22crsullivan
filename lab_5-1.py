8547b2e15a4a92c086bd61b73294562a4d54169a
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-10-27 10:16:40 -0400

    Submit Labs 5-1 and 5-2

---
+++ lab_5-1.py
@@ -0,0 +1,8 @@
+# Author: CRS 10/27/21
+school = "Fairfield Prep"
+first_half = school[:9]
+second_half = school[9:]
+print(first_half)
+print(second_half)
+school2 = first_half + second_half
+print(school2)
---
+++ lab_5-2.py
@@ -0,0 +1,9 @@
+# Author: CRS 10/27/21
+word = input("Please enter the first word.")
+word2 = input("Please enter the second word.")
+if word > word2:
+    print("The first word is greater than the second.")
+elif word == word2:
+    print("The words are equal.")
+else:
+    print("The second word is greater than the first.")
