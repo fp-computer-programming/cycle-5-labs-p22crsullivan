2a7dfff7f8926ff869d10962c8f0670f3796ad73
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-10-29 10:44:44 -0400

    Submit Labs 6-1 and 6-2

---
+++ lab_6-1.py
@@ -0,0 +1,6 @@
+# Author: CRS 10/29/21
+word = "flibbertigibbet"
+print(word.find("t"))
+
+sentence = "I wish, I wish, I was a fish."
+print(sentence.split(","))
---
+++ lab_6-2.py
@@ -0,0 +1,5 @@
+# Author: CRS 10/29/21
+person1 = input("Please enter person 1's name.")
+person2 = input("Please enter person 2's name.")
+sentence = "Hello, {0}. My name is {1}.".format(person1, person2)
+print(sentence)
